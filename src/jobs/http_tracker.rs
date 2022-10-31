6a760d8c0ac905c669c5d5cf4f941bc54eef2aa6
Cameron Garnham <me@da2ce7.com>
2022-10-31 16:50:51 +0100

    more work

--- src/jobs/http_tracker.rs
+++ src/jobs/http_tracker.rs
@@ -5,9 +5,7 @@ use std::sync::Arc;
 use log::{error, info, warn};
 use tokio::task::JoinHandle;
 
-use crate::errors::{FilePathError, ServiceSettingsError, SettingsError, TlsSettingsError};
-use crate::settings::old_settings::HttpTrackerConfig;
-use crate::settings::ServiceSetting;
+use crate::errors::{FilePathError, ServiceSettingsError, TlsSettingsError};
 use crate::tracker::tracker::TorrentTracker;
 use crate::{HttpServer, HttpServiceSettings, TlsServiceSettings};
 
@@ -15,18 +13,8 @@ pub fn start_http_job(settings: &HttpServiceSettings, tracker: Arc<TorrentTracke
     tokio::spawn(async move {
         let http_tracker = HttpServer::new(tracker);
 
-        match settings.tls {
-            Some(tls) => {
-                info!("Starting HTTP Server \"{}\" on: {} (TLS)", settings.name, settings.socket);
-                http_tracker
-                    .start_tls(settings.socket, tls.cert_file_path, tls.key_file_path)
-                    .await;
-            }
-            None => {
-                info!("Starting HTTP Server \"{}\" on: {}", settings.name, settings.socket);
-                http_tracker.start(settings.socket).await;
-            }
-        }
+        info!("Starting HTTP Server \"{}\" on: {}", settings.display_name, settings.socket);
+        http_tracker.start(settings.socket).await;
     })
 }
 
@@ -34,121 +22,14 @@ pub fn start_tls_job(settings: &TlsServiceSettings, tracker: Arc<TorrentTracker>
     tokio::spawn(async move {
         let http_tracker = HttpServer::new(tracker);
 
-        match settings.tls {
-            Some(tls) => {
-                info!("Starting HTTP Server \"{}\" on: {} (TLS)", settings.name, settings.socket);
-                http_tracker
-                    .start_tls(settings.socket, tls.cert_file_path, tls.key_file_path)
-                    .await;
-            }
-            None => {
-                info!("Starting HTTP Server \"{}\" on: {}", settings.name, settings.socket);
-                http_tracker.start(settings.socket).await;
-            }
-        }
+        info!(
+            "Starting HTTP Server \"{}\" on: {} (TLS)",
+            settings.display_name, settings.socket
+        );
+        http_tracker
+            .start_tls(settings.socket, settings.certificate_file_path, settings.key_file_path)
+            .await;
     })
-}
-
-fn get_tracker_settings(config: &HttpTrackerConfig) -> Result<Option<HttpServerSettings>, SettingsError> {
-    let empty_string = &"".to_string();
-    let is_enabled = config.enabled.unwrap_or_default();
-
-    let http_server: String = "HTTP Server".to_string();
-
-    let name = match get_name(config.display_name.as_ref().unwrap_or(empty_string)) {
-        Ok(name) => {
-            info!("Info: Loading Config for HTTP Server: \"{name}\".");
-            Some(name)
-        }
-        Err(error) => {
-            let server_error = error.handel();
-
-            if !is_enabled {
-                warn!("Warning: {}.", server_error.to_string());
-                None
-            } else {
-                error!("Error: {}!", server_error.to_string());
-                return Err(server_error);
-            }
-        }
-    };
-
-    // Not going to continue without a name.
-    if name.is_none() {
-        return Ok(None);
-    };
-
-    let socket = match get_socket(config.bind_address.as_ref().unwrap_or(empty_string)) {
-        Ok(socket) => {
-            info!("Info: HTTP Server \"{}\" uses socket: \"{socket}\".", name.clone().unwrap());
-            Some(socket)
-        }
-        Err(error) => {
-            let server_error = error.handel();
-
-            if !is_enabled {
-                warn!("Warning: {}.", server_error.to_string());
-                None
-            } else {
-                error!("Error: {}!", server_error.to_string());
-                return Err(server_error);
-            }
-        }
-    };
-
-    let tls_config = if config.ssl_enabled.unwrap_or_default() {
-        match get_tls_config(
-            config.ssl_cert_path.as_ref().unwrap_or(empty_string),
-            config.ssl_key_path.as_ref().unwrap_or(empty_string),
-        ) {
-            Ok(tls_config) => {
-                info!(
-                    "Info: HTTP Server \"{}\" uses TLS Certificate: \"{}\".",
-                    name.clone().unwrap(),
-                    tls_config.cert_file_path.display().to_string(),
-                );
-                info!(
-                    "Info: HTTP Server \"{}\" uses TLS Key: \"{}\".",
-                    name.clone().unwrap(),
-                    tls_config.cert_file_path.display().to_string(),
-                );
-                Some(tls_config)
-            }
-            Err(error) => {
-                let server_error = handel_http_tls_config_error(&error, &"ttest".to_owned(), &ServiceSetting::default());
-
-                if !is_enabled {
-                    warn!("Warning: {}.", server_error.to_string());
-                    None
-                } else {
-                    error!("Error: {}!", server_error.to_string());
-                    return Err(server_error);
-                }
-            }
-        }
-    } else {
-        None
-    };
-
-    if is_enabled {
-        Ok(Some(HttpServerSettings {
-            name: name.unwrap(),
-            socket: socket.unwrap(),
-            tls: tls_config,
-        }))
-    } else {
-        Ok(None)
-    }
-}
-
-}
-
-fn get_name(name: &String) -> Result<String, ServiceSettingsError> {
-    if !name.is_empty() {
-        Ok(name.clone())
-    } else {
-        Err(ServiceSettingsError::UnnamedService)
-    }
 }
 
 fn get_socket(bind_addr: &String) -> Result<SocketAddr, ServiceSettingsError> {
